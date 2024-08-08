dcae78a714986c9dd9a7678ae5760a474ea4b2f5
guillgalvan <guillermo.galvan@globallynx.com>
2024-08-07 19:20:52 -0600

    translate demo to spanish and update demo web app

--- notebook/Demo-1/webservice/webapp/app/routes.py
+++ notebook/Demo-1/webservice/webapp/app/routes.py
@@ -3,6 +3,11 @@ from flask_login import login_user, current_user, logout_user, login_required
 from app import db, bcrypt
 from app.forms import RegistrationForm, LoginForm, ExpenseForm
 from app.models import User, Expense
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 bp = Blueprint('routes', __name__)
 
@@ -10,6 +15,7 @@ bp = Blueprint('routes', __name__)
 @bp.route("/home")
 @login_required
 def home():
+    logger.debug("Home route accessed by user: %s", current_user.username)
     return render_template('home.html')
 
 @bp.route("/register", methods=['GET', 'POST'])
@@ -23,7 +29,13 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
+        logger.debug("New user registered: %s", user.username)
         return redirect(url_for('routes.login'))
+    else:
+        for fieldName, errorMessages in form.errors.items():
+            for err in errorMessages:
+                flash(f'{fieldName} - {err}', 'danger')
+                logger.error(f'Error in {fieldName}: {err}')
     return render_template('register.html', title='Register', form=form)
 
 @bp.route("/login", methods=['GET', 'POST'])
@@ -36,13 +48,16 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
+            logger.debug("User logged in: %s", user.username)
             return redirect(next_page) if next_page else redirect(url_for('routes.home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
+            logger.warning("Login failed for email: %s", form.email.data)
     return render_template('login.html', title='Login', form=form)
 
 @bp.route("/logout")
 def logout():
+    logger.debug("User logged out: %s", current_user.username)
     logout_user()
     return redirect(url_for('routes.home'))
 
@@ -55,9 +70,11 @@ def expenses():
         db.session.add(expense)
         db.session.commit()
         flash('Your expense has been added!', 'success')
+        logger.debug("Expense added: %s", expense.description)
         return redirect(url_for('routes.expenses'))
     expenses = Expense.query.filter_by(owner=current_user)
     total = sum(expense.amount for expense in expenses)
+    logger.debug("Expenses page accessed by user: %s", current_user.username)
     return render_template('expenses.html', title='Expenses', form=form, expenses=expenses, total=total)
 
 @bp.route("/delete_expense/<int:expense_id>")
@@ -65,8 +82,10 @@ def expenses():
 def delete_expense(expense_id):
     expense = Expense.query.get_or_404(expense_id)
     if expense.owner != current_user:
+        logger.warning("Unauthorized expense deletion attempt by user: %s", current_user.username)
         abort(403)
     db.session.delete(expense)
     db.session.commit()
     flash('Your expense has been deleted!', 'success')
+    logger.debug("Expense deleted: %s", expense.description)
     return redirect(url_for('routes.expenses'))
